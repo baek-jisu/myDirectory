5e0820d5bcf6503cf42882fb0e56b663dda5140b
cmocmo <>
2022-09-07 18:36:33 +0900

    guguclass

---
+++ GuguClass.java
@@ -0,0 +1,14 @@
+package test08;
+
+public class GuguClass {
+
+	public static void main(String[] args) {
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				System.out.print(i+"*"+j+"="+i*j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
